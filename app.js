49a4906b52b9d7ef8101211026b3f867c7014d09
Kalmeera <kalmera@gmx.de>
2025-03-20 11:17:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
-const wordEl = document.querySelector(".word")
-const oldWordsEl = document.querySelector(".old-words")
-
-//Wortliste_Array
-let words = [];
-let currentword = "";
-let previousWords = [];
-
-
-
-fetch("words.txt").then(function(response){
-   return response.text();
-})
-.then(function(text){
-    words = text.split(',');
-});
-
-//Button klick
-function onClick(){
-    if(currentword){
-        previousWords.push(currentword);
-        oldWordsEl.innerHTML = previousWords.join(', ');
-    }
-    currentword = getRandomWord();
-    wordEl.innerHTML = currentword;
-}
-
-function getRandomNumber(maxNumber){
-    return Math.floor(Math.random() * maxNumber);
-}
-
-function getRandomWord(){
-    return words[getRandomNumber(words.length)]
-}
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
